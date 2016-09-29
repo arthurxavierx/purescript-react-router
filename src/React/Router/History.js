bc03ec961b696266f869e7a3c254c74684422faf
Arthur Xavier <arthur.xavierx@gmail.com>
2016-09-29 10:37:59 -0300

    Fix historyRouter not handling initial route on page load

--- src/React/Router/History.js
+++ src/React/Router/History.js
@@ -2,7 +2,7 @@
 
 exports.watchHistory = function(handler) {
   return function() {
-    handler(typeof window !== 'undefined' ? window.location.pathname : '');
+    handler(typeof window !== 'undefined' ? window.location.pathname : '')();
     if (typeof window !== 'undefined') {
       window.onpopstate = function () {
         handler(window.location.pathname);
